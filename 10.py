8a90fe727fd4c1f9ee98c4c428f8def2433b5d58
ppvn <pop0vn3322@gmail.com>
2024-05-02 09:12:10 +0700

    a

--- 10.py
+++ 10.py
@@ -1,1 +1,10 @@
-t= int(input())
+t = (float(input()))
+count = 0
+while True:
+    temp = float(input())
+    if temp == 0:
+        break
+    if temp < t:
+        count += 1
+    t = temp
+print(count)
